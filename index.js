ddcbed1c05053ccd3d6e6ef8bbc7eba9ae77d64a
Gad-Iradufasha <legmeekmc4@gmail.com>
2025-02-27 23:59:11 +0200

    Loading movies.json data

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 4002;
 
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Running on http://localhost:${PORT}`);
